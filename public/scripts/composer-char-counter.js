aaa3458d56f22b9a64869b69d661283d5e743a09
Bo Yang Yu <boyangyu93@gmail.com>
2023-07-10 21:46:47 -0400

    create tweeter page with styling

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", function() {
+    let total = $(this).val();
+    let remaining = 140 - total.length;
+    $(this).closest(".new-tweet").find(".counter").html(remaining);
+    if (remaining < 0) {
+      $(this).closest(".new-tweet").find(".counter").addClass('red-color');
+    } else {
+      $(this).closest(".new-tweet").find(".counter").removeClass('red-color');
+    }
+  })
+});
